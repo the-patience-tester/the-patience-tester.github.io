074cd11c6b9c175565ab98c704dca08ccd0bd9b0
Peter <peter.simons@n-able.com>
2023-10-15 23:42:33 +0100

    Fixes download cv pdf formatting and moves the button to the nav bar

--- automation-sandbox/src/app/header-nav/header-nav.component.ts
+++ automation-sandbox/src/app/header-nav/header-nav.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import {NavigationEnd, Router} from "@angular/router";
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-header-nav',
@@ -7,7 +9,42 @@ import {Router} from "@angular/router";
   styleUrls: ['./header-nav.component.css']
 })
 export class HeaderNavComponent {
+  showDownload: boolean = true;
   constructor(private router: Router) {}
+  /// This is being used indirectly to determine if the download button should be shown.
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = event.url;
+        console.log('Current Route: ', currentRoute);
+        if (currentRoute === '/calculator') {
+          this.showDownload = false;
+        }
+        else {
+          this.showDownload = true;
+        }
+      }
+    });
+  }
+
+  async OnDownloadPdfClick() {
+    const content = document.getElementById('main-cv');
+    if (content !== null) {
+      try {
+        const canvas = await html2canvas(content);
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF.default('p', 'mm', 'a4');
+
+        pdf.addImage(imgData, 'PNG', 0, 0, 210, 297); // 210x297 mm for A4 page size
+        pdf.save('petersimonscv.pdf');
+      } catch (error) {
+        alert('Error: Could not create the PDF.');
+      }
+    } else {
+      alert('Error: Could not find CV content to download.');
+    }
+  }
+
   navigateToCalculator() {
     this.router.navigate(['/calculator']);
   }
--- automation-sandbox/src/app/landing/landing.component.ts
+++ automation-sandbox/src/app/landing/landing.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import * as jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-landing',
@@ -10,20 +8,4 @@ import html2canvas from 'html2canvas';
 })
 export class LandingComponent {
   constructor(private router: Router) {}
-
-  OnDownloadPdfClick() {
-    const content = document.getElementById('main-cv');
-    if (content !== null) {
-      html2canvas(content).then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF.default('p', 'mm', 'a4');
-
-        pdf.addImage(imgData, 'PNG', 0, 0, 210, 297); // 210x297 mm for A4 page size
-        pdf.save('petersimonscv.pdf');
-      });
-    }
-    else {
-      alert('Error: Could not find CV content to download.');
-    }
-  }
 }
